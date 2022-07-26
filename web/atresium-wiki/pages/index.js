3c6d5b35b25cc782cbee3d33cc9a0845ea7da861
JeremyVerweij <jetroltje@gmail.com>
2022-07-26 22:39:15 +0200

    Update index.js

--- web/atresium-wiki/pages/index.js
+++ web/atresium-wiki/pages/index.js
@@ -1,4 +1,4 @@
 var temp = document.getElementById("headButton");
 temp.addEventListener('click', function(){
-    window.location.replace("");
+    window.location.replace("https://JeremyVerweij.github.io/web/Atresium-wiki/index.html");
 })
