ecd8ae432cbc4236cd70e373bf2070076fa5f3f1
JeremyVerweij <jetroltje@gmail.com>
2022-08-30 14:53:15 +0200

    i changed stuff

--- web/atresium-wiki/pages/index.js
+++ web/atresium-wiki/pages/index.js
@@ -1,4 +1,11 @@
 var temp = document.getElementById("headButton");
+var dir = localStorage.getItem("dir");
 temp.addEventListener('click', function(){
-    window.location.replace("../index.html");
+    window.location.replace(dir + "/web/atresium-wiki/index.html");
+})
+
+temp = document.getElementById("headButton2");
+temp.addEventListener('click', function(){
+    //window.location.replace(dir + "/web/atresium-download-page/index.html");
+    window.open(dir + "/web/atresium-download-page/index.html", '_blank');
 })
