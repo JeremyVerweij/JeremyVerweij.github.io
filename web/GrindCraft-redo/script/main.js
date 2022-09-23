4222415fc51ee434b9e375a5d1a12085f7c219d1
JeremyVerweij <jetroltje@gmail.com>
2022-09-23 20:18:51 +0200

    ?

--- web/GrindCraft-redo/script/main.js
+++ web/GrindCraft-redo/script/main.js
@@ -1,1 +1,18 @@
 "use strict";
+window.addEventListener('load', (event) => {
+    var newBTN = document.getElementById("newBTN");
+    var loadBTN = document.getElementById("loadBTN");
+    var openModsScreen = document.getElementById("openModsScreen"); //menu
+    var loadsave = document.getElementById("loadsave");
+    var modInput = document.getElementById("modInput");
+    var newGame = document.getElementById("newGame"); //menu
+    var gameName = document.getElementById("gameName");
+    var choseMods = document.getElementById("choseMods");
+    var play = document.getElementById("play");
+    var lons = document.getElementById("loadOrNewScreen"); //menu
+    var gameMain = document.getElementById("game"); //menu
+    //lons.style.display = "hidden";
+    //game.style.display = "hidden";
+    //openModsScreen.style.display = "hidden";
+    //newGame.style.display = "hidden";
+});
---
+++ web/GrindCraft-redo/script/main.ts
@@ -0,0 +1,17 @@
+window.addEventListener('load', (event) => {
+    var newBTN:any = document.getElementById("newBTN");
+    var loadBTN:any = document.getElementById("loadBTN");
+    var openModsScreen:any = document.getElementById("openModsScreen");//menu
+    var loadsave:any = document.getElementById("loadsave");
+    var modInput:any = document.getElementById("modInput");
+    var newGame:any = document.getElementById("newGame");//menu
+    var gameName:any = document.getElementById("gameName");
+    var choseMods:any = document.getElementById("choseMods");
+    var play:any = document.getElementById("play");
+    var lons:any = document.getElementById("loadOrNewScreen");//menu
+    var gameMain:any = document.getElementById("game");//menu
+    //lons.style.display = "hidden";
+    //game.style.display = "hidden";
+    //openModsScreen.style.display = "hidden";
+    //newGame.style.display = "hidden";
+});
