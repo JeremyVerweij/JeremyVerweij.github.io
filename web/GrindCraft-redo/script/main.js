c98c573b20880cb09d7dea1ab795bf2296920383
JeremyVerweij <jetroltje@gmail.com>
2022-09-29 21:31:07 +0200

    a

--- web/GrindCraft-redo/script/main.js
+++ web/GrindCraft-redo/script/main.js
@@ -1,18 +1,16 @@
 "use strict";
-window.addEventListener('load', (event) => {
-    var newBTN = document.getElementById("newBTN");
-    var loadBTN = document.getElementById("loadBTN");
-    var openModsScreen = document.getElementById("openModsScreen"); //menu
-    var loadsave = document.getElementById("loadsave");
-    var modInput = document.getElementById("modInput");
-    var newGame = document.getElementById("newGame"); //menu
-    var gameName = document.getElementById("gameName");
-    var choseMods = document.getElementById("choseMods");
-    var play = document.getElementById("play");
-    var lons = document.getElementById("loadOrNewScreen"); //menu
-    var gameMain = document.getElementById("game"); //menu
-    //lons.style.display = "hidden";
-    //game.style.display = "hidden";
-    //openModsScreen.style.display = "hidden";
-    //newGame.style.display = "hidden";
-});
+var gameElem = document.getElementById("game"); //main
+var newGame = document.getElementById("newGame"); //main
+var gameName = document.getElementById("gameName");
+var choseMods = document.getElementById("choseMods");
+var play = document.getElementById("play");
+var openModsScreen = document.getElementById("openModsScreen"); //main
+var loadsave = document.getElementById("loadsave");
+var modInput = document.getElementById("modInput");
+var loadOrNewScreen = document.getElementById("loadOrNewScreen"); //main
+var newBTN = document.getElementById("newBTN");
+var loadBTN = document.getElementById("loadBTN");
+gameElem.style = "display:none;";
+newGame.style = "display:none;";
+openModsScreen.style = "display:none;";
+//loadOrNewScreen.style = "display:none;"
--- web/GrindCraft-redo/script/main.ts
+++ web/GrindCraft-redo/script/main.ts
@@ -1,17 +1,16 @@
-window.addEventListener('load', (event) => {
-    var newBTN:any = document.getElementById("newBTN");
-    var loadBTN:any = document.getElementById("loadBTN");
-    var openModsScreen:any = document.getElementById("openModsScreen");//menu
-    var loadsave:any = document.getElementById("loadsave");
-    var modInput:any = document.getElementById("modInput");
-    var newGame:any = document.getElementById("newGame");//menu
-    var gameName:any = document.getElementById("gameName");
-    var choseMods:any = document.getElementById("choseMods");
-    var play:any = document.getElementById("play");
-    var lons:any = document.getElementById("loadOrNewScreen");//menu
-    var gameMain:any = document.getElementById("game");//menu
-    //lons.style.display = "hidden";
-    //game.style.display = "hidden";
-    //openModsScreen.style.display = "hidden";
-    //newGame.style.display = "hidden";
-});
+var gameElem:any = document.getElementById("game");//main
+var newGame:any = document.getElementById("newGame");//main
+var gameName:any = document.getElementById("gameName");
+var choseMods:any = document.getElementById("choseMods");
+var play:any = document.getElementById("play");
+var openModsScreen:any = document.getElementById("openModsScreen");//main
+var loadsave:any = document.getElementById("loadsave");
+var modInput:any = document.getElementById("modInput");
+var loadOrNewScreen:any = document.getElementById("loadOrNewScreen");//main
+var newBTN:any = document.getElementById("newBTN");
+var loadBTN:any = document.getElementById("loadBTN");
+
+gameElem.style = "display:none;"
+newGame.style = "display:none;"
+openModsScreen.style = "display:none;"
+//loadOrNewScreen.style = "display:none;"
